9fb633e48e294da9e05b0ba1c0bf97dcfd8b952c
lichao@cnhnkj.com <lichao@cnhnkj.com>
2019-10-08 17:26:51 +0800

    添加项目文件。

---
+++ QuestionForm/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿using System;
+
+namespace QuestionForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "导入题库";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(251, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "随机出题";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(403, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 70);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(614, 208);
+            this.textBox1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(116, 305);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(30, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "A";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(212, 305);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(30, 16);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "B";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(312, 305);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(30, 16);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "C";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(415, 305);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(30, 16);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "D";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(514, 305);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(30, 16);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "E";
+            this.checkBox5.UseVisualStyleBackColor = true;
+
+            this.checkBox1.Visible = false;
+            this.checkBox2.Visible = false;
+            this.checkBox3.Visible = false;
+            this.checkBox4.Visible = false;
+            this.checkBox5.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(655, 348);
+            this.button4.Name = "下一题";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "下一题";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "答题器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ QuestionForm/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;   //引用MySql
+
+namespace QuestionForm
+{
+    public partial class MainForm : Form
+    {
+        int pageSize = 10;
+        String connectMysql = "server=127.0.0.1;port=3306;user=root;password=lichao12; database=mysql;";
+        DataRowCollection hotRows;
+        int currentRowNo;
+        int hitCount = 0;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            // server=127.0.0.1/localhost 代表本机，端口号port默认是3306可以不写
+            MySqlConnection conn = new MySqlConnection(connectMysql);
+            conn.Open();
+            //创建mysql数据库表
+            CreatedMysqlTable();
+            ReadFromFile();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.checkBox1.Visible = false;
+            this.checkBox2.Visible = false;
+            this.checkBox3.Visible = false;
+            this.checkBox4.Visible = false;
+            this.checkBox5.Visible = false;
+            var sql = "SELECT * FROM questions ORDER BY RAND() LIMIT  "+pageSize;
+            DataSet dataSet=MySqlHelper.ExecuteDataset(connectMysql, sql);
+            currentRowNo = 0;
+            hotRows = dataSet.Tables[0].Rows;
+            this.button4.Visible = true;
+            this.button4.Text = "下一题";
+            refreshQuestion();
+
+        }
+
+        private void refreshQuestion()
+        {
+            this.textBox1.Text = hotRows[currentRowNo].ItemArray[1].ToString();
+            this.textBox1.Text += Environment.NewLine;
+            this.textBox1.Text += Environment.NewLine;
+            this.textBox1.Text += Environment.NewLine;
+            String options = hotRows[currentRowNo].ItemArray[2].ToString();
+            this.textBox1.Text += options;
+            this.checkBox1.Visible = true;
+            this.checkBox2.Visible = true;
+            this.checkBox3.Visible = true;
+            this.checkBox4.Visible = true;
+            this.checkBox1.CheckState = CheckState.Unchecked;
+            this.checkBox2.CheckState = CheckState.Unchecked;
+            this.checkBox3.CheckState = CheckState.Unchecked;
+            this.checkBox4.CheckState = CheckState.Unchecked;
+            if (options.IndexOf('E') > 0)
+            {
+                this.checkBox5.Visible = true;
+            }
+            else {
+                this.checkBox5.Visible = false;
+            }
+            this.checkBox5.CheckState = CheckState.Unchecked;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            currentRowNo++;
+            if (currentRowNo == pageSize - 1)
+            {
+                checkAnswer(currentRowNo-1);
+                this.button4.Text = "查看结果";
+            }
+            else if (currentRowNo == pageSize)
+            {
+                this.textBox1.Text += "本次答题完毕！";
+                this.textBox1.Text += Environment.NewLine;
+                this.textBox1.Text += Environment.NewLine;
+                this.textBox1.Text += "一共题目" + pageSize + ",答对 " + hitCount + " 题";
+                this.button4.Visible = false;
+                this.checkBox1.Visible = false;
+                this.checkBox2.Visible = false;
+                this.checkBox3.Visible = false;
+                this.checkBox4.Visible = false;
+                this.checkBox5.Visible = false;
+            }
+            else {
+                checkAnswer(currentRowNo-1);
+                refreshQuestion();
+            }
+                
+        }
+
+        private void checkAnswer(int index)
+        {
+            String correctAnswers= hotRows[index].ItemArray[3].ToString();
+            String filledAnswers = String.Empty;
+            if (this.checkBox1.Checked)
+                filledAnswers+="A";
+            if (this.checkBox2.Checked)
+                filledAnswers += "B";
+            if (this.checkBox3.Checked)
+                filledAnswers += "C";
+            if (this.checkBox4.Checked)
+                filledAnswers += "D";
+            if (this.checkBox5.Checked)
+                filledAnswers += "E";
+            if (correctAnswers.Equals(filledAnswers))
+                hitCount += 1;
+        }
+
+        private void ReadFromFile()
+        {
+            String filePath = "D:\\test.xls";
+            String tablename = getTableName(filePath);
+            //括号中为表格地址  
+            DataSet ds = ExcelToDataSet(filePath, tablename);
+            var sql = "INSERT IGNORE INTO questions(content,options,answers) VALUE(?content,?options,?answers)";
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                string content = ds.Tables[tablename].Rows[i]["试题内容"].ToString();  //Rows[i]["col1"]表示i行"col1"字段  
+                string options = ds.Tables[tablename].Rows[i]["试题选项"].ToString();
+                string answers = ds.Tables[tablename].Rows[i]["答案"].ToString();
+                int answers_len = answers.Length;
+                string pattern = @"[(（]\s*[ABCDE]+\s*[）)]";
+                foreach (Match match in Regex.Matches(content, pattern))
+                {
+                    // Execute the query  
+                    foreach (char c in match.Value)
+                    {
+                        if (!"ABCDE".Contains(c))
+                            continue;
+                        content = content.Replace(c, ' ');
+                        if (answers_len < 1)
+                        {
+                            answers += c;
+                        }
+                    }
+                    
+                }
+                MySqlHelper.ExecuteNonQuery(connectMysql, sql, new MySqlParameter("content", content), 
+                     new MySqlParameter("options", options),
+                     new MySqlParameter("answers", answers));
+            }
+            int a = 1;
+        }
+
+        private bool CreatedMysqlTable()
+        {
+            #region 创建mysql数据库表
+
+            using (var Conn = new MySqlConnection(connectMysql))
+            {
+                Conn.Open();
+
+                String createStatement = @" CREATE TABLE `questions` (                  `Id` int(4) NOT NULL AUTO_INCREMENT,                  `content` varchar(255) COLLATE utf8_bin DEFAULT NULL,                  `options` varchar(255) COLLATE utf8_bin DEFAULT NULL,                  `answers` varchar(10) COLLATE utf8_bin DEFAULT NULL,                  PRIMARY KEY (`Id`)                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ";
+                using (MySqlCommand cmd = new MySqlCommand(createStatement, Conn))
+                {
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return false;
+                    }
+                }
+                #endregion
+            }
+        }
+
+        //函数用来读取一个excel文件到DataSet集中  
+        public static DataSet ExcelToDataSet(string filename, string tableName)
+        {
+            OleDbConnection myConn = getConn(filename);
+            myConn.Open();
+            tableName = getTableName(filename);
+            string strCom = " SELECT * FROM [" + tableName + "]";
+            //获取Excel指定Sheet表中的信息
+            OleDbDataAdapter myCommand = new OleDbDataAdapter(strCom, myConn);
+            DataSet ds;
+            ds = new DataSet();
+
+            myCommand.Fill(ds, tableName);
+            myConn.Close();
+            return ds;
+        }
+
+        private static OleDbConnection getConn(String filename)
+        {
+            //获取文件扩展名
+            string strExtension = System.IO.Path.GetExtension(filename);
+            OleDbConnection myConn = null;
+            switch (strExtension)
+            {
+                case ".xls":
+                    myConn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filename + ";" + "Extended Properties=\"Excel 8.0;HDR=yes;IMEX=1;\"");
+                    break;
+                case ".xlsx":
+                    myConn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filename + ";" + "Extended Properties=\"Excel 12.0;HDR=yes;IMEX=1;\"");
+                    //此连接可以操作.xls与.xlsx文件 (支持Excel2003 和 Excel2007 的连接字符串) 
+                    //"HDR=yes;"是说Excel文件的第一行是列名而不是数，"HDR=No;"正好与前面的相反。"IMEX=1 "如果列中的数据类型不一致，使用"IMEX=1"可必免数据类型冲突。 
+                    break;
+                default:
+                    myConn = null;
+                    break;
+            }
+            if (myConn == null)
+            {
+                return null;
+            }
+            return myConn;
+        }
+
+        private static string getTableName(String fileName)
+        {
+            OleDbConnection myConn = getConn(fileName);
+            //读取文件
+            DataTable dataTab = new DataTable();
+            myConn.Open();
+            //获取表名的一种方式
+            dataTab = myConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });//获得表头
+            String tableName = dataTab.Rows[0]["Table_Name"].ToString();
+            myConn.Close();
+            return tableName;
+        }
+
+        
+    }
+}
